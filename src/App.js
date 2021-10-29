22568b7d70a38b64476bd3deb06ade5bb11fe160
mserhatyuksel <mserhat.yksl@gmail.com>
2021-10-29 17:21:29 +0300

    Added state and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
-
+import { useState } from "react";
+import Filters from "./components/Filters";
+import Input from "./components/Input";
+import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
 function App() {
+    const [todos, setTodos] = useState(["Test Todo"]);
+    const addTodo = (todo) => {
+        setTodos([...todos, todo]);
+    };
     return (
         <div className="App">
-            <h1 className="title">todos</h1>
+            <Input addTodo={addTodo} />
+            <TodoList todos={todos} />
+            <Filters />
+            <Footer />
         </div>
     );
 }
